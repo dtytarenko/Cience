02e9c694dc7cfb94c3b2bd7e8e87d534af4f06d8
http404ntfnd <dmutro.tutarenko@gmail.com>
2019-08-22 12:17:44 +0300

    end tablet style

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,26 @@
-console.log(1);
+document.querySelector('iframe').onload = function(){
+
+    console.log('iframe loaded');
+    let iframe = document.getElementsByTagName('iframe');
+    const iframeArray = Array.from(iframe)
+    console.log(iframeArray)
+    console.log(iframeArray[1]);
+    let cssLink = document.createElement('link');
+    cssLink.href = 'style.css'; 
+    cssLink.rel = 'stylesheet'; 
+    cssLink.type = 'text/css;';
+
+    var calendar = iframeArray[1];
+
+    var iframeDoc = calendar.contentWindow.document;
+
+    if (iframeDoc.readyState == 'complete') {
+        iframeDoc.body.style.backgroundColor = 'green';
+    }
+    calendar.onload = function() {
+        var iframeDoc2 = calendar.contentWindow.document;
+        iframeDoc2.body.style.backgroundColor = 'orange';
+    }
+    var bsdf = window.frames[0].frameElement.contentWindow.targetFunction(); 
+    console.log(bsdf)
+};
