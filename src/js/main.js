9658409bba777d6c8d213ab353b6cf0290a01ec3
http404ntfnd <dmutro.tutarenko@gmail.com>
2019-08-22 17:37:28 +0300

    end style for desktop

--- src/js/main.js
+++ src/js/main.js
@@ -3,24 +3,32 @@ document.querySelector('iframe').onload = function(){
     console.log('iframe loaded');
     let iframe = document.getElementsByTagName('iframe');
     const iframeArray = Array.from(iframe)
-    console.log(iframeArray)
-    console.log(iframeArray[1]);
-    let cssLink = document.createElement('link');
-    cssLink.href = 'style.css'; 
-    cssLink.rel = 'stylesheet'; 
-    cssLink.type = 'text/css;';
-
     var calendar = iframeArray[1];
+    console.log(calendar)
+    calendar.setAttribute('sandbox', 'allow-scripts');
+    calendar.setAttribute('seamless', '');
+    
 
-    var iframeDoc = calendar.contentWindow.document;
 
-    if (iframeDoc.readyState == 'complete') {
-        iframeDoc.body.style.backgroundColor = 'green';
-    }
-    calendar.onload = function() {
-        var iframeDoc2 = calendar.contentWindow.document;
-        iframeDoc2.body.style.backgroundColor = 'orange';
-    }
-    var bsdf = window.frames[0].frameElement.contentWindow.targetFunction(); 
-    console.log(bsdf)
+//     var cssLink = document.createElement("link");
+//     cssLink.href = "style.css"; 
+//     cssLink.rel = "stylesheet"; 
+//     cssLink.type = "text/css"; 
+
+
+
+//     var addCssToIframe = function() {
+//         if ($('#iframeID').contents().find("head") != undefined) {
+//             $('#iframeID')
+//                     .contents()
+//                     .find("head")
+//                     .append(
+//                             '<link rel="stylesheet" href="xui/css/iframe.css" type="text/css" />');
+//             $interval.cancel(addCssInterval);
+//         }
+//     };
+//     var addCssInterval = $interval(addCssToIframe, 500, 0, false);
+
+//     setTimeout(getHead, 5000)
+    
 };
